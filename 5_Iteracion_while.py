cf77cbd3fbf696c09ced6c7529a839c97f8dd4d4
Avksentije <a.klozevits@gmail.com>
2023-01-07 10:57:00 -0600

    Initial commit

--- 5_Iteracion_while.py
+++ 5_Iteracion_while.py
@@ -1,7 +1,7 @@
 #Tabla del 7 usando while
 
 
-i = 1
+i = 0
 while i <= 10:
     table = i * 7
     print("7 x " + str(i), "= " + str(table))
--- 6_Iteracion_for.py
+++ 6_Iteracion_for.py
@@ -1,6 +1,6 @@
 #Tabla del 7 usando for
 
-for j in range(1, 11):
+for j in range(0, 17):
     print ("7 x " + str(j), "= " + str(j*7)) #por pares: texto + tomar el valor de la variable o la operación
     j = j+1
 
